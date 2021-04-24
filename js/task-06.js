ff47d256bfc00521dc31eed883d7cf2aab6afa86
Aleksandra Maksymets <omelchenko.aleksandra@gmail.com>
2021-04-24 22:33:00 +0100

    improved code at mentor's advice

--- js/task-06.js
+++ js/task-06.js
@@ -3,18 +3,22 @@ const inputEl = document.querySelector('#validation-input');
 
 inputEl.addEventListener('blur', onInputChange);
 
+function addAndRemoveClass(classToAdd, classToRemove) {
+    inputEl.classList.add(classToAdd);
+    inputEl.classList.remove(classToRemove);
+ };
+
 function onInputChange(event) {
     const dataDefaultLength = inputEl.getAttribute('data-length');
 
     if (event.currentTarget.value.length > dataDefaultLength) {
         console.log('event.currentTarget.value.length', event.currentTarget.value.length);
-       
-        inputEl.classList.add('invalid');
-        inputEl.classList.remove('valid');
+        addAndRemoveClass('invalid', 'valid');
+    
     } else {
         console.log('event.currentTarget.value.length', event.currentTarget.value.length);
-        inputEl.classList.remove('invalid');
-        inputEl.classList.add('valid');
+       addAndRemoveClass('valid', 'invalid' );
+        
     }
     console.log(inputEl);
 };
